2a931ff1377acf15f8c2e5cdf87ab66f475c2e8a
Muyiwaa <enochmwilliams@gmail.com>
2022-04-12 04:41:50 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a program that prints _putchar
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+
+	return (0);
+}
